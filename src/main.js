7dd4d0abeb8a8af5389a0c852d16319d70e0d95b
xufei <xu.fei@outlook.com>
2016-11-20 10:48:13 +0800

    2.1.3版本可以不用import publish和empty

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import { Observable } from 'rxjs/Observable'
 import { Subscription } from 'rxjs/Subscription'
-import 'rxjs/add/operator/publish'
-import 'rxjs/add/observable/empty'
+
 import VueRx from 'vue-rx'
 
 import App from './App.vue'
